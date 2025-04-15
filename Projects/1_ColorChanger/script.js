c3496a9e1569c2aed8753e953ed208b4b1f5a197
sajal-pakira <uniquesajal6@gmail.com>
2025-04-15 11:28:55 +0000

    project 1 Color Change completed

---
+++ Projects/1_ColorChanger/script.js
@@ -0,0 +1,31 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    console.log(e);
+    console.log(e.target);
+    switch (e.target.id) {
+      case 'grey':
+        body.style.backgroundColor = e.target.id;
+      case 'white':
+        body.style.backgroundColor = e.target.id;
+      case 'blue':
+        body.style.backgroundColor = e.target.id;
+      case 'yellow':
+        body.style.backgroundColor = e.target.id;
+    }
+    // if (e.target.id == 'grey') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id == 'white') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id == 'blue') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id == 'yellow') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+  });
+});
